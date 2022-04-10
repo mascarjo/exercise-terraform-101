17649748f0f644115f29aa92ec161351b900d4c3
Johans <johansmascardo@hotmail.com>
2022-04-10 15:36:18 -0400

    update

--- 3-terraformlab/main.tf
+++ 3-terraformlab/main.tf
@@ -16,32 +16,20 @@ variable "name" {
   default     = "myinstance"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t3.micro"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.image_id
-  instance_type = var.instance_type
-
+  ami           = var.ami
+  instance_type = "t2.micro"
   tags = {
     Name = var.name
   }
+}
+
+variable "ami" {
+  description = "Amazon Machine Image"
+  default = "ami-07eaf2ea4b73a54f6"
+}
+
+variable "name" {
+  description = "I like names"
+  
 }
