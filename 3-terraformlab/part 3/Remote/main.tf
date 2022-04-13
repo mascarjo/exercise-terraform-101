ac8d3882b2d09d8bd5b3162a0594f00e7c26d6ff
Johans <johansmascardo@hotmail.com>
2022-04-13 13:15:22 -0400

    update

--- 3-terraformlab/part 3/Remote/main.tf
+++ 3-terraformlab/part 3/Remote/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "tf-stack" {
-  source = "github.com:mascarjo/terraform-lab-03-simplestack.git"
-  project_tags =  {
-    Name       = "terraform-makevpc"
-    Owner      = "Johans Mascardo"
-    Purpose    = "Testing"
-    CostCenter = "0001"
-  }
+module "vpcstack" {
+  source = "github.com/mascarojo/Lab3VPC"
 }
